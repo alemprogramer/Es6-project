b95510512e44c3f21d63d12de57a713ff4cbf2f1
alemprogramer <alemhhaisb@gmail.com>
2020-09-27 18:44:48 +0600

    uploade dist

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-document.write("<h1><center>This  project create for<br> ES6<br> code test ...</center><h1>")
+//document.write("<h1><center>This  project create for<br> ES6<br> code test ...</center><h1>")
+var array=[1,4,5,8,6,7,9,54];
+array.forEach(element => {
+    console.log(element);
+    
+    
+});
+console.log(0/1);
 
 
-document.write("")
+
+function playStop(){
+    console.log(Math.round(Math.random()*50 +1))
+    //Math.round(Math.random()*50 +1)
+}
+
+
+
+
+// document.write("")
+
+//  const arr1=[1,3,4,5];
+//  const arr2=[5,6,4,6];
+//  const arr3=arr1+arr2;
+
+//  //document.write('arr3');
+
+
+// const name="hasib"
+// document.write(name)
+// console.log(name)
